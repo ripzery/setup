865b0e4d3e6d9a625ef487175803842a73c54c91
euro <phuchit@omisego.co>
2020-07-26 13:54:35 +0700

    Add condition to install vim

--- debian/setup.sh
+++ debian/setup.sh
@@ -1,6 +1,19 @@
 #!/bin/sh
 
-# Install VIM
+# Update apt packages
 
 apt-get update
-echo "y" | apt-get install vim
+
+# Install VIM
+
+if ! type "$vim" > /dev/null; then
+  echo "######## Installing VIM... ########"
+  echo "y" | apt-get install vim
+fi
+
+# Install Mosh server
+
+## Enable firewall over UDP ports
+echo "-A INPUT -p udp --dport 60000:61000 -j ACCEPT" > iptables.firewall.rules
+
+

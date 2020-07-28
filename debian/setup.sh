2ddfe442415a375667f333637f5b59ecfe2322df
euro <phuchit@omisego.co>
2020-07-28 12:08:19 +0700

    Install nvm

--- debian/setup.sh
+++ debian/setup.sh
@@ -89,6 +89,29 @@ if ! command -v ag > /dev/null; then
   install silversearcher-ag
 fi
 
+# Install node
+
+#if ! command -v node > /dev/null; then
+#  print_header "Installing node..."
+#  install node
+#fi
+#
+#if ! command -v yarn > /dev/null; then
+#  print_header "Installing yarn..."
+#  install yarn
+#fi
+#
+#if ! command -v npm > /dev/null; then
+#  print_header "Installing npm..."
+#  install npm
+#fi
+
+# Install nvm
+
+if ! command -v nvm > /dev/null; then
+  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+fi
+
 # Install VIM
 
 if ! command -v vim > /dev/null; then
@@ -98,8 +121,11 @@ if ! command -v vim > /dev/null; then
   print_header "Installing VIM plug..."
   curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-  vim +PlugInstall +qall > /dev/null
+      vim +PlugInstall +qall > /dev/null
 fi
+
+
+
 
 chsh -s $(which zsh)
 echo "Use zsh!"
